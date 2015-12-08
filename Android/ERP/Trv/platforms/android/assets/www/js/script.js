b2bf9ac33b7fde1acba618de8f38807cadd712b2
Matheus Forte Amarante <matheus@ats.com.br>
2015-12-08 18:52:30 -0200

    CONSULTA PEÇA

--- Android/ERP/Trv/platforms/android/assets/www/js/script.js
+++ Android/ERP/Trv/platforms/android/assets/www/js/script.js
@@ -170,3 +170,11 @@ function OkTecladoAndroid3(lcTxAtua, lcTxFocu, lcAhClic) {
         return true;
     });
 }
+
+function escapeRegExp(str) {
+    return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+}
+
+function replaceAll(str, find, replace) {
+    return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
