3b43849086c23b2ceb45d62bd15d80dfa1653cc3
Matheus Forte Amarante <matheus@ats.com.br>
2016-01-25 15:38:04 -0200

    APLICATIVO ATS

---
+++ Android/ERP/Ats/platforms/android/assets/www/js/script.js
@@ -0,0 +1,25 @@
+﻿function OkTecladoAndroidFocu(lcTxAtua, lcTxFocu) {
+    $("#" + lcTxAtua).on("keyup", function (e) {
+        var theEvent = e || window.event;
+        var keyPressed = theEvent.keyCode || theEvent.which;
+        if (keyPressed == 13) {
+            document.getElementById(lcTxFocu).focus();
+        }
+        return true;
+    });
+}
+
+function OkTecladoAndroidBotao(lcTxAtua, lcAhClic) {
+    $("#" + lcTxAtua).on("keyup", function (e) {
+        var theEvent = e || window.event;
+        var keyPressed = theEvent.keyCode || theEvent.which;
+        if (keyPressed == 13) {
+            document.getElementById(lcAhClic).focus();
+
+            if (typeof document.getElementById(lcAhClic).onclick == "function") {
+                document.getElementById(lcAhClic).onclick.apply(document.getElementById(lcAhClic));
+            }
+        }
+        return true;
+    });
+}
